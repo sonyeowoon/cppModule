de62a4854c090532430fbd51e11685bf9a646fc1
Sanghee Seo <sangseo@c1r3s8.42gyeongsan.kr>
2025-08-21 13:12:40 +0900

    cpp00-ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangseo <sangseo@student.42gyeongsan.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 13:22:55 by sangseo           #+#    #+#             */
+/*   Updated: 2025/08/13 22:22:17 by sangseo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	int i = 1;
+	while (av[i])
+	{
+		int j = 0;
+		while (av[i][j])
+		{
+			av[i][j] = toupper(av[i][j]);
+			j++;
+		}
+		std::cout << av[i];
+		i++;
+	}
+	std::cout << std::endl;
+}
